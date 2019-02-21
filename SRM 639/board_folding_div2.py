3b3f9380d4fb05e448f62fd71568bece76079c73
Yashawant Retharekar <yashwant.retharekar@gmail.com>
2019-02-21 09:15:09 +0530

    [WIP] board folding

--- SRM 639/board_folding_div2.py
+++ SRM 639/board_folding_div2.py
@@ -1,22 +1,53 @@
-class BoardFoldingDiv2():
-    def rotate(self, paper):
+class BoardFoldingDiv2:
+    @staticmethod
+    def rotate(paper):
         row = len(paper)
         column = len(paper[0])
 
-        ans = ["" for i in range(row)]
+        ans = []
+        for i in range(column):
+            ans.append("")
 
         for i in range(row):
             for j in range(column):
-                ans[i] = ans[i] + paper[j][i]
+                ans[j] = ans[j] + paper[i][j]
 
         return tuple(ans)
 
-    def howManyVirtical(self, paper):
-        
-        return len(paper)
+    def flip(self, paper):
+        return paper[::-1]
 
+    def solve_top(self, paper):
+        paper_rows = len(paper)
+        paper_columns = len(paper[0])
+        fold = [False for _ in range(paper_rows)]
+        fold[0] = True
+        ans = False
+
+        for i in range(paper_rows):
+            if fold[i]:
+                for h in range((paper_rows - i)//2):
+                    for m in range(1, h):
+                        ans = True
+                        for j in range(paper_columns):
+                            ans = ans and (paper[i+m][j] == paper[i+h+h-m-1][j])
+                
+                if ans:
+                    fold[i+h] = True
+
+        return fold
+
+    def how_many_vertical(self, paper):
+        ans_t = self.solve_top(paper)
+        ans_b = self.solve_top(self.flip(paper))
+        ans = 0
+        for i in range(len(ans_t)):
+            for j in range(len(ans_b)-i):
+                ans = ans + 1 if ans_t[i] and ans_b[j] else ans
+
+        return ans
 
     def howMany(self, paper):
-        ansV = self.howManyVirtical(paper)
-        ansH = self.howManyVirtical(self.rotate(paper))
-        return ansV*ansH
+        ans_v = self.how_many_vertical(paper)
+        ans_h = self.how_many_vertical(BoardFoldingDiv2.rotate(paper))
+        return ans_v*ans_h
--- SRM 639/main.py
+++ SRM 639/main.py
@@ -2,5 +2,17 @@ from board_folding_div2 import BoardFoldingDiv2
 
 if __name__ == "__main__":
     ans = BoardFoldingDiv2()
-    a = ("147", "258", "369")
+    a = ("0","0","0","1","0","0")
     print(ans.howMany(a))
+
+# ["0110","1001","1001","0110"] 9
+# ["1111111","1111111"]	84
+
+# {"1111111", "1111111"}
+# Returns: 84
+
+# {"0110", "1001", "1001", "0110"}
+# Returns: 9
+
+# {"0", "0", "0", "1", "0", "0"}
+# Returns: 6
